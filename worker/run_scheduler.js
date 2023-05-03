ec53c096616e413e090389b8fd58ca12d4956ad1
Aditya Fidiantyo <aditya.fidiantyo@commsult.id>
2023-05-03 15:09:03 +0700

    update scheduler to every 5 minute

--- worker/run_scheduler.js
+++ worker/run_scheduler.js
@@ -2,7 +2,7 @@ var cron = require('node-cron');
 var shell = require('shelljs');
 
 
-cron.schedule('* * * * *', async () => {
+cron.schedule('*/5 * * * *', async () => {
     await shell.rm('-rf', 'allure-results');
     await shell.exec("npm run test")
 });
